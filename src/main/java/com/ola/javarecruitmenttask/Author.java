8e74b80e445d780717ff54895b99da97c6089196
Ola00 <aleksandra.beczkowska00@gmail.com>
2023-03-16 17:32:37 +0100

    .

---
+++ src/main/java/com/ola/javarecruitmenttask/Author.java
@@ -0,0 +1,27 @@
+package com.ola.javarecruitmenttask;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Data
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+
+}
---
+++ src/main/java/com/ola/javarecruitmenttask/BookCategory.java
@@ -0,0 +1,5 @@
+package com.ola.javarecruitmenttask;
+
+public enum BookCategory {
+
+}
